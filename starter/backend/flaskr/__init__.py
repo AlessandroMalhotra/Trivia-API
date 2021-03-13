deb45b929709656c3f32a001370822f99b44bd86
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-03-13 14:00:52 +0000

    Update __init__.py

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -85,8 +85,7 @@ def create_app(test_config=None):
   
  
   # DONE
-  ''' @TODO: Create an endpoint to DELETE question using a question ID. 
-  TEST: When you click the trash icon next to a question, the question will be removed. This removal will persist in the database and when you refresh the page. '''
+  ''' @TODO: Create an endpoint to DELETE question using a question ID.  '''
   
   @app.route('/questions/<int:question_id>', methods=['DELETE'])
   def delete_question(question_id):
@@ -113,9 +112,7 @@ def create_app(test_config=None):
 
  
    # DONE
-  ''' @TODO: Create an endpoint to POST a new question, which will require the question and answer text, category, and difficulty score.
-  TEST: When you submit a question on the "Add" tab, the form will clear and the question will appear at the end 
-  of the last page of the questions list in the "List" tab. '''
+  ''' @TODO: Create an endpoint to POST a new question, which will require the question and answer text, category, and difficulty score. '''
 
   @app.route('/questions', methods=['POST'])
   def create_question():
@@ -149,9 +146,7 @@ def create_app(test_config=None):
 
   # DONE
   ''' @TODO: Create a POST endpoint to get questions based on a search term. It should return any questions for whom the search term 
-  is a substring of the question.
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. Try using the word "title" to start.'''
+  is a substring of the question.'''
 
   @app.route('/questions/search', methods=['POST'])
   def search_questions():
@@ -177,8 +172,7 @@ def create_app(test_config=None):
 
   
   # DONE
-  ''' @TODO: Create a GET endpoint to get questions based on category. 
-  TEST: In the "List" tab / main screen, clicking on one of the categories in the left column will cause only questions of that category to be shown. '''
+  ''' @TODO: Create a GET endpoint to get questions based on category. '''
 
   @app.route('/categories/<int:id>/questions', methods=['GET'])
   def questions_based_on_category(id):
@@ -200,9 +194,7 @@ def create_app(test_config=None):
   
    # DONE
   ''' @TODO: Create a POST endpoint to get questions to play the quiz. This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, if provided, and that is not one of the previous questions. 
-  TEST: In the "Play" tab, after a user selects "All" or a category, one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. '''
+  and return a random questions within the given category, if provided, and that is not one of the previous questions.  '''
 
   @app.route('/quizzes', methods=['POST'])
   def create_quiz():
